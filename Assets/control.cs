c37f8a7eec151d87f4b586085b90050596d78889
LucaNicoleta <61989219+LucaNicoleta@users.noreply.github.com>
2020-10-06 07:49:04 +0300

    .

---
+++ Assets/control.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //Vector3 Dist = Vector3.Distance(Camera.main.transform.position, transform.position) if (Dist <= TurnDist)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("camera_is_near", true);
+    }
+}
